92aed1598d8acb0a54ce358d409aef03d3b9b977
Miranael <foltzl@essilor.fr>
2016-01-10 14:45:59 +0100

    Check Scripts

--- Scripts/CreateDB.sql
+++ Scripts/CreateDB.sql
@@ -1,14 +1,11 @@
-drop database if exists dbusers;
-create database dbusers;
+DROP database if EXISTS dbusers;
+CREATE database dbusers;
 use dbusers;
 
-CREATE TABLE `users` (
-	`user_id` INT(5) NOT NULL AUTO_INCREMENT, 
-	`username` VARCHAR(50), 
-	`password` VARCHAR(50) ,
+CREATE TABLE 'Users' (
+	'user_id' INT(5) NOT NULL AUTO_INCREMENT, 
+	'username' VARCHAR(50), 
+	'password' VARCHAR(50) ,
 	PRIMARY KEY(`user_id`), 
 	INDEX(`username`)
 	);
-	
-	
- 

3fe6080d8db642f71a4e0ca5f04f56957dc472a3
ebrahimaD <diallo.elhadj.ibrahima@gmail.com>
2016-01-10 17:13:01 +0100

    client consol 1

--- src/main/client/view/ClientConsol.java
+++ src/main/client/view/ClientConsol.java
@@ -1,30 +1,31 @@
 package main.client.view;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class ClientConsol {
 
-	public static void main(String[] args) {
-		
-		boolean flag = false;
+	public static void main(String[] args) throws IOException {
 
+		BufferedReader keyboard= new BufferedReader(
+				new InputStreamReader(System.in));
+		
 		System.out.println("*********************************");
-		System.out.println("* Welcome To EatMeIfYouCan !	*");
+		System.out.println("* Welcome To Eat Me If You Can !*");
 		System.out.println("*********************************");
 		System.out.println("");
-		
-		while(!flag){
-			System.out.print("Console: ");
-			
-			String user =keyboard.readLine();
-			switch(s){
-			case "exit": 
-				connection.interrupt();
-				System.exit(0);
-				break;
-			default: System.out.println("Help: \n exit - arreter le serveur \n help - l'affichage d'aide" );
-			}
 
-	}
-	
+		String s = keyboard.readLine();
+		switch(s){
+		case "exit": 
+			System.exit(0);
+			
+			break;
+		default: System.out.println("Help: \n exit - arreter le serveur \n help - l'affichage d'aide" );
+
+		}
+
 	}
 
 }

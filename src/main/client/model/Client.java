47790d72416064a8d773210ebaf4a2223f2c8d62
ebrahimaD <diallo.elhadj.ibrahima@gmail.com>
2016-01-10 12:57:47 +0100

    connexion client

--- src/main/client/model/Client.java
+++ src/main/client/model/Client.java
@@ -1,7 +1,48 @@
 package main.client.model;
 
+
+
+
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.net.ConnectException;
+import java.net.Socket;
+import java.io.OutputStream;
+
+
 public class Client {
 	private final int CREATE_ACCOUNT=0;
-	private final int AUTHENTICATE=1;
-	
+	private final static int AUTHENTICATE=1;
+
+	//public ConnectionBean Authentificate(){
+
+	//	ConnectionBean c= new ConnectionBean(AUTHENTICATE, "user", "pas");
+	//	return c;
+	//}
+
+	//public static  ConnectionBean CreateAccount(){
+	//	ConnectionBean c= new ConnectionBean(CREATE_ACCOUNT, "test", "mdp");
+	//	return c;
+	//}
+
+	public static void main(String[] args) throws IOException, InterruptedException, ConnectException {
+		Socket c=null;
+		while(c == null){
+			try{
+				c = new Socket("localhost",8080);
+				System.out.println("Client \n local port: "+c.getLocalPort()+", Target port: "+c.getPort());
+
+			}catch(ConnectException e){
+				Thread.sleep(1000);	
+			}
+		}
+		OutputStream os = c.getOutputStream();
+		ObjectOutputStream oos = new ObjectOutputStream(os);
+
+		ConnectionBean conect = new ConnectionBean(AUTHENTICATE, "user", "pas");
+		oos.writeObject(conect);
+		oos.close();
+		os.close();
+		c.close();
+	}
 }
--- src/main/client/model/ConnectionBean.java
+++ src/main/client/model/ConnectionBean.java
@@ -1,7 +1,9 @@
 package main.client.model;
+import java.io.Serializable;
+
 import main.client.model.ConnectionBean;
 
-public class ConnectionBean {
+public class ConnectionBean implements Serializable{
 	private final int CREATE_ACCOUNT=0;
 	private final int AUTHENTICATE=1;
 	private int type;

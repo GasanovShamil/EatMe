a933eff0f91e31c9fb8002869a8dde33de5bea3b
GasanovShamil <gasanov.sh@gmail.com>
2016-01-10 00:48:23 -0800

    ajout de ServerAthenticationThread et connexion avec MySQL

--- src/main/server/threads/ServerAuthenticationThread.java
+++ src/main/server/threads/ServerAuthenticationThread.java
@@ -4,6 +4,7 @@ import java.net.Socket;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Properties;
@@ -13,22 +14,24 @@ public class ServerAuthenticationThread extends Thread {
 	//private Statement statement=null;
 	//private ResultSet resultSet=null;
 	
-	public static void main(String[] args) throws SQLException{
+	public static void main(String[] args) throws SQLException, ClassNotFoundException{
+		//Class.forName("com.mysql.jdbc.Driver");
 		Connection dbConnection=null;
 		Statement statement=null;
 		ResultSet resultSet=null;
 		Properties prop = new Properties();
-        
-		prop.put("characterEncoding","UTF8");
+        prop.put("characterEncoding","UTF8");
 		prop.put("user", "root");
 		prop.put("password", "root");
 		
 		dbConnection=DriverManager.getConnection("jdbc:mysql://localhost:3306/dbusers",prop);
 		
 		statement=dbConnection.createStatement();
-		String query="SELECT * FROM users WHERE username=shamil";
+		String query="SELECT * FROM users WHERE username='shamil'";
+		
 		resultSet=statement.executeQuery(query);
-		System.out.println(resultSet.getString("username"));
+		if(resultSet.next()) System.out.println(resultSet.getString("username"));
+			
 	}
 	
 	

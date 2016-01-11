6bd938d68f930c77ee021c264f9895d16f4690f1
GasanovShamil <gasanov.sh@gmail.com>
2016-01-10 17:06:03 -0800

    ezf

--- src/main/server/threads/ServerAuthenticationThread.java
+++ src/main/server/threads/ServerAuthenticationThread.java
@@ -2,6 +2,7 @@ package main.server.threads;
 
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.Socket;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -17,19 +18,29 @@ public class ServerAuthenticationThread extends Thread {
 	private Statement statement = null;
 	private ResultSet resultSet = null;
 	private Properties prop;
+	ObjectInputStream input;
+	ObjectOutputStream output;
 	ConnectionBean connectionBean;
 	Socket s;
 
 	public ServerAuthenticationThread(Socket s) {
 		this.s = s;
+		try {
+			input = new ObjectInputStream(s.getInputStream());
+			output= new ObjectOutputStream(s.getOutputStream());
+			connectionBean=(ConnectionBean)input.readObject();			
+		} catch (IOException | ClassNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
 	}
 
 	public void run() {
-
+		
 		try {
-			ObjectInputStream ois = new ObjectInputStream(s.getInputStream());
-			connectionBean=(ConnectionBean)ois.readObject();
-			System.out.println(connectionBean.getLogin());
+			
+			connectionBean=(ConnectionBean)input.readObject();
+			
 		} catch (IOException | ClassNotFoundException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
@@ -43,17 +54,23 @@ public class ServerAuthenticationThread extends Thread {
 		try {
 			dbConnection = DriverManager.getConnection("jdbc:mysql://localhost:3306/dbusers", prop);
 			statement = dbConnection.createStatement();
+			
 			String query = "SELECT * FROM users WHERE username=\""+connectionBean.getLogin()+"\"";
 			resultSet = statement.executeQuery(query);
 			if (resultSet.next()){
-				System.out.println("Hello "+resultSet.getString("username")+"");
+				System.out.println("Hello "+resultSet.getString("username")+"\n Console: ");
 			}else{
-				System.out.println("There is no user with this name: "+connectionBean.getLogin());
+				System.out.println("There is no user with this name: "+connectionBean.getLogin()+"\n Console: ");
 			}
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+	
+	public boolean athenticate(ConnectionBean connectionBean){
+		
+		return true;
 	}
 
 }

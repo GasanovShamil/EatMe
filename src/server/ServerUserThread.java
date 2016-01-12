fc499162c312536eacc4fb304c1ef7c17bef6a6b
GasanovShamil <gasanov.sh@gmail.com>
2016-01-11 19:11:24 -0800

    fix

--- src/server/ServerUserThread.java
+++ src/server/ServerUserThread.java
@@ -4,25 +4,32 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
+import client.Message;
 import game.User;
 
-public class ServerUserThread extends Thread{
+public class ServerUserThread extends Thread {
 	private User user;
 	ObjectInputStream input;
 	ObjectOutputStream output;
-	
+
 	public ServerUserThread(User user) {
-		this.user=user;
+		this.user = user;
 		try {
-			this.input=user.getInput();
-			this.output=user.getOutput();
+			this.input = user.getInput();
+			this.output = user.getOutput();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+
 	}
-	
-	public void run(){
+
+	public void run() {
+		try {
+			Message message = (Message) input.readObject();
+		} catch (ClassNotFoundException | IOException e) {
+			e.printStackTrace();
+		}
 		
+
 	}
 }

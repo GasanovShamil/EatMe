d49f3a618cd1cb49cd31d180ec5fa9096f1bea73
GasanovShamil <gasanov.sh@gmail.com>
2016-01-11 18:39:01 -0800

    fix connexion

--- src/server/ServerUserThread.java
+++ src/server/ServerUserThread.java
@@ -1,12 +1,25 @@
 package server;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
 import game.User;
 
 public class ServerUserThread extends Thread{
 	private User user;
+	ObjectInputStream input;
+	ObjectOutputStream output;
 	
-	public ServerUserThread(User user){
+	public ServerUserThread(User user) {
 		this.user=user;
+		try {
+			this.input=user.getInput();
+			this.output=user.getOutput();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
 	}
 	
 	public void run(){
